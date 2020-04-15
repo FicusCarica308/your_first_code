0e4af79c9bc4035261cf5a8aee186adbf97597e2
FicusCarica308 <63696144+FicusCarica308@users.noreply.github.com>
2020-04-15 00:37:39 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 int main()
 {
   int i;
-  for (i = 0; i < 10; i++)
+  for (i = 0; i < 32; i++) //changed from 10 to 32
   {
-    printf("Holberton\n");
+    printf("Hello Holberton\n"); //add Hello
   }
   return 0;
 }
